f0929541f4b00ecdc72ade307129d6ea481ea16d
Natan Santos <natansantos888@hotmail.com>
2020-07-31 19:29:30 -0300

    Instalando e Configurando o Styled-Components

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {
+    A,
+} from './style.js'
+
+function Header(){
+    return(
+        <A>Hello World!</A>
+    );
+};
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import GlobalStyled from './pages/GlobalStyled.js'
+
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyled />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
